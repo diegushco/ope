006169fbb0993a44d1d1774e86e6087415045557
diegushco <zorro8815@gmail.com>
2015-03-18 12:18:11 -0430

    cambio minimo

--- guardarEventoCenso1.php
+++ guardarEventoCenso1.php
@@ -4,7 +4,7 @@
 				$consulta="INSERT INTO cs_evento(Nombre,Id_TipoEvento) VALUES ('".$_REQUEST["evento"]."',(select Id_TipoEvento from cs_tipo_evento where Nombre='".$_REQUEST["tipo"]."'))";
 				$sql=mysql_query ($consulta,$conexion);		
 				
-				echo $consulta;
+				//echo $consulta;
 				if($sql){
 						echo "Se guardo correctamente.";
 				}else{
