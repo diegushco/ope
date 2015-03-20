33a546bb0104e9f707274abadb8df46f1e517c4b
root <root@DiegoPC>
2015-03-19 21:37:55 -0430

    subiendo cambios

--- man_eliminaTiEvento.php
+++ man_eliminaTiEvento.php
@@ -3,6 +3,7 @@
 			include("conexion.php");	
 			$consulta="delete from cs_tipo_evento WHERE Nombre='".$_REQUEST["viejo"]."'";
 		//echo $consulta;
+			//echo $consulta;
 			$sql=mysql_query($consulta,$conexion);  
 			if($sql){ //cuentaVieja CuentaNueva centroV centroN
 				echo "Se elimino correctamente";				
